eaf2c3f7427a8fe3d269e901f533fd281959d85a
Matheus <matheus.sam.10@gmail.com>
2024-11-30 22:23:14 -0300

    Check if user is logged in

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,6 +1,112 @@
-const backendAddress = 'http://127.0.0.1:8000/api/tasks/';
+const backendAddress = 'http://127.0.0.1:8000/api/';
+
+function addTaskButtonListener() {
+    const addTaskButton = document.getElementById('add-task');
+    addTaskButton.addEventListener('click', saveTask);
+}
+
+function handleLogin() {
+    const username = (document.getElementById('username')).value;
+	const password = (document.getElementById('password')).value;
+    fetch(backendAddress + 'auth/token', {
+        method: 'POST',
+        body: JSON.stringify({
+            'username': username,
+            'password': password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+	})
+	.then((response) => {
+		if(response.ok) {
+			return response.json();
+		} else {
+			if(response.status == 401) {
+                alert('Usuário ou senha inválidos.');
+			}
+			throw new Error('Falha na autenticação');
+		}
+	})
+	.then((data) => {
+        const token = data.token;
+        localStorage.setItem('token', token);
+        window.location.replace('index.html');
+    })
+	.catch(erro => { 
+        console.log(erro)
+    })
+}
+
+function handleLogout() {
+    const token = localStorage.getItem('token');
+    fetch(backendAddress + 'auth/token', {
+        method: 'DELETE',
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.ok) window.location.replace('index.html');
+        else alert('Erro ' + response.status);
+    })
+    .catch(erro => { console.log(erro); })
+}
+
+function makeLoginLogoutButtonListener() {
+    console.log("listen");
+    document.body.addEventListener('click', evento => {
+		evento.preventDefault();
+		if(evento.target.id == 'make-login') {
+            handleLogin();
+        }
+        if(evento.target.id == 'make-logout') {
+            handleLogout();
+        }
+	});
+}
+
+function checkLogin() {
+    window.addEventListener('load', () => {
+        const token = localStorage.getItem('token');
+        // const token = 'fece423ca932c966c7935ad9c0f2b96684ca4b7f';
+        fetch(backendAddress + 'auth/token', {
+            method: 'GET',
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(response => {
+            response.json().then(data => {
+                const usuario = data;
+                if(response.ok) {
+                    let objDiv = (document.getElementById('logged'));
+                    objDiv.classList.remove('hide');
+                    objDiv.classList.add('show');
+                    objDiv = (document.getElementById('unlogged'));
+                    objDiv.classList.remove('show');
+                    objDiv.classList.add('hide');
+                } else {
+                    let objDiv = (document.getElementById('unlogged'));
+                    objDiv.classList.remove('hide');
+                    objDiv.classList.add('show');
+                    objDiv = (document.getElementById('logged'));
+                    objDiv.classList.remove('show');
+                    objDiv.classList.add('hide');
+                }
+                const spanElement = document.getElementById('nome_user');
+                spanElement.innerHTML = 'Olá, ' + usuario.username + '!';
+            })
+        })
+        .catch(erro => {
+            console.log('[setLoggedUser] deu erro: ' + erro);
+        });
+    });
+}
+
 function loadTasks() {
-    fetch(backendAddress)
+    fetch(backendAddress + 'tasks/')
         .then(response => response.json())
         .then(tasks => {
         const list = document.getElementById('task-list');
@@ -13,6 +119,7 @@ function loadTasks() {
         console.error('Erro ao carregar tarefas:', error);
     });
 }
+
 function renderTask(task) {
     const li = document.createElement('li');
     const checkmark = document.createElement('div');
@@ -34,6 +141,7 @@ function renderTask(task) {
     const list = document.getElementById('task-list');
     list.appendChild(li);
 }
+
 function saveTask() {
     const input = document.getElementById('new-task');
     const task = { title: input.value, completed: false };
@@ -41,7 +149,7 @@ function saveTask() {
         alert('A tarefa não pode estar vazia!');
         return;
     }
-    fetch(backendAddress, {
+    fetch(backendAddress + 'tasks/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(task),
@@ -54,8 +162,9 @@ function saveTask() {
         console.error('Erro ao salvar tarefa:', error);
     });
 }
+
 function toggleComplete(id, completed) {
-    fetch(`${backendAddress}${id}/`, {
+    fetch(`${backendAddress + 'tasks/'}${id}/`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ completed }),
@@ -65,11 +174,12 @@ function toggleComplete(id, completed) {
         console.error('Erro ao atualizar tarefa:', error);
     });
 }
+
 function deleteTask(id) {
     if (!confirm('Tem certeza de que deseja apagar esta tarefa?')) {
         return;
     }
-    fetch(`${backendAddress}${id}/`, {
+    fetch(`${backendAddress + 'tasks/'}${id}/`, {
         method: 'DELETE',
     })
         .then(() => loadTasks())
@@ -77,8 +187,10 @@ function deleteTask(id) {
         console.error('Erro ao apagar tarefa:', error);
     });
 }
+
 document.addEventListener('DOMContentLoaded', () => {
-    const addTaskButton = document.getElementById('add-task');
-    addTaskButton.addEventListener('click', saveTask);
+    addTaskButtonListener();
+    makeLoginLogoutButtonListener();
+    checkLogin();
     loadTasks();
 });
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,7 +1,113 @@
-const backendAddress = 'http://127.0.0.1:8000/api/tasks/';
+const backendAddress = 'http://127.0.0.1:8000/api/';
+
+function addTaskButtonListener() {
+    const addTaskButton = document.getElementById('add-task');
+    addTaskButton.addEventListener('click', saveTask);
+}
+
+function handleLogin() {
+    const username: String = (document.getElementById('username') as HTMLInputElement).value;
+	const password: String = (document.getElementById('password') as HTMLInputElement).value;
+    fetch(backendAddress + 'auth/token', {
+        method: 'POST',
+        body: JSON.stringify({
+            'username': username,
+            'password': password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+	})
+	.then((response: Response) => {
+		if(response.ok) {
+			return response.json();
+		} else {
+			if(response.status == 401) {
+                alert('Usuário ou senha inválidos.');
+			}
+			throw new Error('Falha na autenticação');
+		}
+	})
+	.then((data: { token: string }) => {
+		const token: string = data.token;
+		localStorage.setItem('token', token);
+		window.location.replace('index.html');
+	})
+	.catch(erro => { 
+        console.log(erro)
+    })
+}
+
+function handleLogout() {
+    const token = localStorage.getItem('token');
+    fetch(backendAddress + 'auth/token', {
+        method: 'DELETE',
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.ok) window.location.replace('index.html');
+        else alert('Erro ' + response.status);
+    })
+    .catch(erro => { console.log(erro); })
+}
+
+function makeLoginLogoutButtonListener() {
+    console.log("listen to login");
+    document.body.addEventListener('click', evento => {
+		evento.preventDefault();
+        const target = evento.target as HTMLElement;
+        if (target && target.id === 'make-login') {
+            handleLogin(); // Call the logout handler
+        }
+        if (target && target.id === 'make-logout') {
+            handleLogout(); // Call the logout handler
+        }
+	});
+}
+
+function checkLogin() {
+    window.addEventListener('load', () => {
+        const token = localStorage.getItem('token');
+        // const token = 'fece423ca932c966c7935ad9c0f2b96684ca4b7f';
+        fetch(backendAddress + 'auth/token', {
+            method: 'GET',
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(response => {
+            response.json().then(data => {
+                const usuario = data;
+                if(response.ok) {
+                    let objDiv = (document.getElementById('logged') as HTMLDivElement);
+                    objDiv.classList.remove('hide');
+                    objDiv.classList.add('show');
+                    objDiv = (document.getElementById('unlogged') as HTMLDivElement);
+                    objDiv.classList.remove('show');
+                    objDiv.classList.add('hide');
+                } else {
+                    let objDiv = (document.getElementById('unlogged') as HTMLDivElement);
+                    objDiv.classList.remove('hide');
+                    objDiv.classList.add('show');
+                    objDiv = (document.getElementById('logged') as HTMLDivElement);
+                    objDiv.classList.remove('show');
+                    objDiv.classList.add('hide');
+                }
+                const spanElement = document.getElementById('nome_user') as HTMLSpanElement;
+                spanElement.innerHTML = 'Olá, ' + usuario.username  + '!';
+            })
+        })
+        .catch(erro => {
+            console.log('[setLoggedUser] deu erro: ' + erro);
+        });
+    });
+}
 
 function loadTasks() {
-    fetch(backendAddress)
+    fetch(backendAddress + 'tasks/')
         .then(response => response.json())
         .then(tasks => {
             const list = document.getElementById('task-list') as HTMLUListElement;
@@ -51,7 +157,7 @@ function saveTask() {
         return;
     }
 
-    fetch(backendAddress, {
+    fetch(backendAddress + 'tasks/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(task),
@@ -66,7 +172,7 @@ function saveTask() {
 }
 
 function toggleComplete(id: number, completed: boolean) {
-    fetch(`${backendAddress}${id}/`, {
+    fetch(`${backendAddress + 'tasks/'}${id}/`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ completed }),
@@ -82,7 +188,7 @@ function deleteTask(id: number) {
         return;
     }
 
-    fetch(`${backendAddress}${id}/`, {
+    fetch(`${backendAddress + 'tasks/'}${id}/`, {
         method: 'DELETE',
     })
         .then(() => loadTasks())
@@ -92,8 +198,8 @@ function deleteTask(id: number) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const addTaskButton = document.getElementById('add-task')!;
-    addTaskButton.addEventListener('click', saveTask);
-
+    addTaskButtonListener();
+    makeLoginLogoutButtonListener();
+    checkLogin();
     loadTasks(); 
 });
